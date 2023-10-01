b9b65113506a8d3eabff644cf468cd2a9c28c918
miche66 <87225548+miche66@users.noreply.github.com>
2023-10-01 02:25:58 +0200

    Installed Iphone emulator, test app is working

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>
+        Any changes you make in your VS Code App.js file will be visible here
+        when you hit save.
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +17,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    paddingRight: 20,
+    paddingLeft: 20,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
